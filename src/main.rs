4d3725e9495be8554ea7585b72035c6ae35114e8
Meenakshi Sundaram <vms20591@gmail.com>
2017-12-17 00:48:07 +0530

    Control Flows - loop

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,6 @@
 fn main() {
-    let print_hello = true;
-
-    // Blocks are also called as "arms"
-    if print_hello {
-        println!("Hello, world!");
+    // Runs forever until stopped
+    loop {
+        println!("main");
     }
-    else {
-        println!("Hello, person!");
-    }
-
-    let number = 10;
-
-    if number < 10 {
-        println!("Lesser");
-    }
-    else if number > 10 {
-        println!("Greater");
-    }
-    else {
-        println!("Equal");
-    }
-
-    // Since "if" is an "expression", it can be used right side of a "statement"
-    // But all blocks should return same type
-    let value = if number < 10 {
-        5
-    } else if number > 10 {
-        6
-    } else {
-        7
-    };
-
-    println!("value is {}", value);
 }
