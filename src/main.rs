f8715fe08667774d892d53a962853f44a1dbc41b
Meenakshi Sundaram <vms20591@gmail.com>
2017-12-17 00:51:30 +0530

    Control Flows - while

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    // Runs forever until stopped
-    loop {
-        println!("main");
+    // Runs till condition holds true
+    let mut number = 3;
+    
+    while number > 0 {
+        println!("number is {}", number);
+        number = number - 1;
     }
 }
